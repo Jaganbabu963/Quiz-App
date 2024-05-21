892fd5656335e8308044be2d116b36f81b417e29
Jaganbabu963 <jagancheemal@gmail.com>
2024-05-21 19:55:15 +0530

    add files

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quiz from './comps/quiz_comps/Quiz'
+
+
+const App = () => {
+  return (
+    <>
+     <Quiz />
+    </>
+  )
+}
+
+export default App
---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,50 @@
+export const data = [
+     {
+          question:"What is the Prabhas New Movie?",
+          option1:"Pushpa",
+          option2:"KALKI 2898D",
+          option3:"Game Changer",
+          option4:"Devara Part-1",
+          ans:2
+     },
+     {
+          question:"Who is the Director of Movie KALKI 2898D ?",
+          option1:"Pushpa",
+          option2:"Sukumar",
+          option3:"Nag Ashwin",
+          option4:"Koratala Shiva",
+          ans:3
+     },
+     {
+          question:"On Which banner KALKI 2898D is releasing ?",
+          option1:"Mythri Movie Makers",
+          option2:"Vyjayanthi Movies",
+          option3:"Sithara Entertainment",
+          option4:"Geetha Arts",
+          ans:2
+     },
+     {
+          question:"What is the Ram Charan New Movie?",
+          option1:"Pushpa",
+          option2:"KALKI 2898D",
+          option4:"Game Changer",
+          option3:"Devara Part-1",
+          ans:4
+     },
+     {
+          question:"What is the Jr.NTR New Movie?",
+          option4:"Pushpa",
+          option2:"KALKI 2898D",
+          option3:"Game Changer",
+          option1:"Devara Part-1",
+          ans:1
+     },
+     {
+          question:"What is the Nani's New Movie?",
+          option4:"saripodha sanivaram",
+          option2:"KALKI 2898D",
+          option3:"Game Changer",
+          option1:"Devara Part-1",
+          ans:4
+     },
+]
